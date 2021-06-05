789db45abecd315a44f691973d4916c4efe96f5f
p7e4 <p7e4@users.noreply.github.com>
2021-06-05 22:15:01 +0800

    update

--- ctfscan/ctfscan.py
+++ ctfscan/ctfscan.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-__version__ = "1.3.3"
+__version__ = "1.3.4"
 
 import sys
 import requests
 import logging
 from multiprocessing.dummy import Pool as ThreadPool
 import argparse
-import os
 
 logging.basicConfig(format="", level=logging.INFO)
 logger = logging.getLogger(__name__)
-
-datapath = os.path.abspath(os.path.dirname(__file__))
 
 def scan(path):
     try:
@@ -32,17 +29,180 @@ def main():
     parser.add_argument("-t", "--thread", default=6, help="thread, default 6")
     global args
     global url
-    global data
     args = parser.parse_args()
     url = args.url[:-1] if args.url.endswith("/") else args.url
-    with open(f"{datapath}/data/data.txt") as f:
-        data = f.read()
 
     pool = ThreadPool(processes=args.thread)
     pool.map(scan, data.split("\n"))
     pool.close()
     pool.join()
 
+data = """\
+/flag
+/flag.php
+/flag.txt
+/.git
+/.git/HEAD
+/.git/index
+/.git/config
+/.git/description
+/README.MD
+/README.md
+/README
+/.gitignore
+/.svn
+/.svn/wc.db
+/.svn/entries
+/.hg
+/.DS_Store
+/WEB-INF/web.xml
+/WEB-INF/src/
+/WEB-INF/classes
+/WEB-INF/lib
+/WEB-INF/database.propertie
+/CVS/Root
+/CVS/Entries
+/.bzr/
+/_viminfo
+/.viminfo
+/phpinfo.php
+/robots.txt
+/test.php
+/.htaccess
+/.bash_history
+/.svn/
+/.git/
+/.hg/
+/.index.php.swp
+/.index.php
+/.index.php~
+/index.php.bak
+/index.php.bak_Edietplus
+/index.php.~1~
+/index.php
+/index.php.rar
+/index.php.zip
+/index.php.7z
+/index.php.tar.gz
+/index.php.txt
+/index.php~
+/index.bak
+/login.php
+/register.php
+/upload.php
+/phpinfo.php
+/ssrf.php
+/a.php
+/b.php
+/c.php
+/d.php
+/e.php
+/f.php
+/g.php
+/h.php
+/i.php
+/j.php
+/k.php
+/l.php
+/m.php
+/n.php
+/o.php
+/p.php
+/q.php
+/r.php
+/s.php
+/t.php
+/u.php
+/v.php
+/w.php
+/x.php
+/y.php
+/z.php
+/0.php
+/1.php
+/2.php
+/3.php
+/4.php
+/5.php
+/6.php
+/7.php
+/8.php
+/9.php
+/www.zip
+/www.rar
+/www.7z
+/www.tar.gz
+/www.tar
+/web.zip
+/web.rar
+/web.7z
+/web.tar.gz
+/web.tar
+/plus
+/0
+/1
+/05/
+/s8qq.txt
+/s8log.txt
+/s8wwwroot.rar
+/s8web.rar
+/dede
+/admin
+/edit
+/Fckeditor
+/ewebeditor
+/bbs
+/Editor
+/manage
+/shopadmin
+/web_Fckeditor
+/login,asp
+/webadmin
+/admin/WebEditor
+/admin/daili/webedit
+/login/
+/database/
+/tmp/
+/manager/
+/manage/
+/web/
+/admin/
+/shopadmin/
+/wp-includes/
+/edit/
+/editor/
+/1.zip
+/1.rar
+/1.7z
+/1.tar.gz
+/1.tar
+/tar.zip
+/tar.rar
+/web1.zip
+/web1.rar
+/123.zip
+/123.rar
+/code.zip
+/code.rar
+/root.zip
+/root.rar
+/wwwroot.zip
+/wwwroot.rar
+/backup.zip
+/backup.rar
+/mysql.bak
+/a.sql
+/b.sql
+/db.sql
+/bdb.sql
+/ddb.sql
+/mysql.sql
+/dump.sql
+/data.sql
+/backup.sql
+/backup.sql.gz
+/backup.sql.bz2
+/backup.zip"""
 
 if __name__ == '__main__':
     main()
